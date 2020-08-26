1f01436589024ef264df4fcdd1c9a05aed6f6e3f
taylorplumer <taylorplumer@gmail.com>
2020-08-25 22:04:51 -0700

    Update Data filepath

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from helpers import *
 from autots.autots import Dashboard
 
-df = load_df('AirPassengers.csv', '%Y-%m')
+df = load_df('Data/AirPassengers.csv', '%Y-%m')
 app = Dashboard(df = df, image='assets/auto-ts-logo.png', title="Air Passengers", m=12)
 #app.load_df('AirPassengers.csv', '%Y-%m')
 app.preprocess()
