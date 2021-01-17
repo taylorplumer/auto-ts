cf970760c609ee90cd242485a92e4839b1e1f598
Taylor Plumer <45341982+taylorplumer@users.noreply.github.com>
2021-01-16 18:09:23 -0800

    Fix app.py title typo

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from helpers import *
 from autots.autots import Dashboard
 
 df = load_df('Data/AirPassengers.csv', '%Y-%m')
-app = Dashboard(df = df, image='assets/auto-ts-logo.png', title="Pierce Discharges", m=12, test_size=.10)
+app = Dashboard(df = df, image='assets/auto-ts-logo.png', title="Air Passengers", m=12, test_size=.10)
 app.preprocess()
 app.main()
